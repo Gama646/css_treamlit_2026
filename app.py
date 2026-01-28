825f576f0b03598f51987a9c846c60d7b451f479
Charles <sibonginkosigama646@gmail.com>
2026-01-29 01:37:23 +0200

    streamlit_app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 17:32:56 2026
+
+@author: BBarsch
+"""
+
+import streamlit as st
+
+st.write("CSS 2026: ")
+
+st.title("My First Streamlit App")
+
+st.write("Hello, Streamlit, day 3")
+
+st.header("Number selection")
+
+number = st.slider("Pick a number", 1, 100)
+st.write(f"You picked: {number}")
+
